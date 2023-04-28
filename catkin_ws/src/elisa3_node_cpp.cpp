78a27c5b391d45e5c544ad4ff569d6cff5e41722
EvaZwetsloot <e.p.m.zwetsloot@student.tudelft.nl>
2023-04-28 13:37:22 +0200

    include functions

--- catkin_ws/src/elisa3_node_cpp.cpp
+++ catkin_ws/src/elisa3_node_cpp.cpp
@@ -73,6 +73,56 @@ double xPosCorr, yPosCorr;
 double robDistTraveled, robDistTraveledPrev, robDeltaDistTraveled;
 ros::Time currentTime, lastTime, currentTimeMap, lastTimeMap;
 
+#define OBSTACLE_THR 50
+
+unsigned char updateRGB(char *red, char *green, char *blue) {
+    static unsigned int i=0;
+    unsigned int rndNum;
+
+    i = (i+1)%65000;  // use to change the rgb leds
+    if(i==0) {
+        rndNum = rand()%400;
+        if(rndNum < 100) {
+            *red = rand()%100;
+            *green = rand()%100;
+            *blue = 0;
+        } else if(rndNum < 200) {
+            *red = rand()%100;
+            *green = 0;
+            *blue = rand()%100;
+        } else if(rndNum < 300) {
+            *red = 0;
+            *green = rand()%100;
+            *blue = rand()%100;
+        } else {
+            *red = rand()%100;
+            *green = rand()%100;
+            *blue = rand()%100;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+void avoidObstacles(unsigned int *prox, char *left, char *right) {
+    int rightProxSum=0, leftProxSum=0;	// sum of proximity values on right or left
+
+    // obstacle avoidance using the 3 front proximity sensors
+    rightProxSum = prox[0]/2 + prox[1];
+    leftProxSum = prox[0]/2 + prox[7];
+
+    rightProxSum /= 5;                 // scale the sum to have a moderate impact on the velocity
+    leftProxSum /= 5;
+    if(rightProxSum > 60) {             // velocity of the motors set to be from -30 to +30
+        rightProxSum = 60;
+    }
+    if(leftProxSum > 60) {
+        leftProxSum = 60;
+    }
+    *right = 30-leftProxSum;     // set the speed to the motors
+    *left = 30-rightProxSum;
+
+}
 
 int main(int argc,char *argv[]) {
 //    double init_xpos, init_ypos, init_theta;
